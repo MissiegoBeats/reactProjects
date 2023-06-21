2c8b89e04e0e60b80ff7f79a317b3fa6e826520c
MissiegoBeats <missiego.andres@gmail.com>
2023-06-21 14:31:07 +0200

    changes

--- clone-web/src/components/Testimony.js
+++ clone-web/src/components/Testimony.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../style-sheets/Testimony.css';
 
-export function Testimony() {
+export function Testimony(props) {
   return (
     <div className="testimony-contanier">
-      <img className="testimony-cliff" src={require('../images/cliff.png')} alt="cliff" height={300} width={300}/>
-      <div className="testimony-text-testimony">
-        <p className="testimony-name">Emma Bostian en Suecia</p>
-        <p className="testimony-job">Ingeniera de SW en Spotify</p>
-        <p className="testimony-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci sagittis eu volutpat odio facilisis mauris sit amet. Mattis enim ut tellus elementum sagittis vitae et leo. At augue eget arcu dictum varius duis at consectetur.</p>
+      <img className="testimony-cliff" src={require(`../images/${props.image}.png`)} alt="cliff" height={300} width={300}/>
+      <div className="testimony-text-container">
+        <p className="testimony-name">{props.name} en {props.country}</p>
+        <p className="testimony-job">{props.job} en {props.company}</p>
+        <p className="testimony-text">{props.text}</p>
       </div>
     </div>
   );
