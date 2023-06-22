b0fe5524f676c85edc8b71cd16028ce28688f5ba
MissiegoBeats <missiego.andres@gmail.com>
2023-06-22 10:27:29 +0200

    Primer proyecto terminado

--- clone-web/src/components/Testimony.js
+++ clone-web/src/components/Testimony.js
@@ -4,10 +4,10 @@ import '../style-sheets/Testimony.css';
 export function Testimony(props) {
   return (
     <div className="testimony-contanier">
-      <img className="testimony-cliff" src={require(`../images/${props.image}.png`)} alt="cliff" height={300} width={300}/>
+      <img className="testimony-cliff" src={require(`../images/${props.image}.png`)} alt="cliff" height={300} width={300} />
       <div className="testimony-text-container">
-        <p className="testimony-name">{props.name} en {props.country}</p>
-        <p className="testimony-job">{props.job} en {props.company}</p>
+        <p className="testimony-name"><strong>{props.name}</strong> en {props.country}</p>
+        <p className="testimony-job">{props.job} en <strong>{props.company}</strong></p>
         <p className="testimony-text">{props.text}</p>
       </div>
     </div>
