2bcb9dc6c1264b8ecaeb49296447ff03fe882143
MissiegoBeats <missiego.andres@gmail.com>
2023-06-22 12:28:08 +0200

    Added buttons

--- clicks-counter/src/App.js
+++ clicks-counter/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
+  
+  const addClick = () => {
+    console.log('handleClick()');
+  };
+
+  const resetCounter = () =>{
+    console.log('resetCounter()');
+  };
+
   return (
     <div className="App">
-      
+      <div className="counter-container">
+        <Button
+          text = 'Click Me'
+          isClickButton = {true}
+          handleClick = {addClick} />
+        <Button
+          text = 'Reset Counter'
+          isClickButton = {false}
+          handleClick = {resetCounter} />
+      </div>
     </div>
   );
 }
---
+++ clicks-counter/src/components/Contador.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Contador({ numClicks }) {
+  return(
+    <div className="counter-container">
+      { numClicks }
+    </div>
+  );
+};
