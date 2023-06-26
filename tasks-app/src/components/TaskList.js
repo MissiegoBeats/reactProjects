8d1e1dc36db516aa1bd5c21fa925986f64166b5f
MissiegoBeats <missiego.andres@gmail.com>
2023-06-26 10:47:19 +0200

    almost finished

--- tasks-app/src/components/TaskList.js
+++ tasks-app/src/components/TaskList.js
@@ -7,7 +7,7 @@ function TaskList() {
 
   const [arrayTasks, setArrayTasks] = useState([])
 
-  const addTask = (task) => {
+  const addTask = task => {
     if (task.text.trim()) {
       task.text = task.text.trim();
       const updatedArrayTasks = [task, ...arrayTasks];
@@ -16,16 +16,26 @@ function TaskList() {
     }
   };
 
-  const completeTask = (id) => {
-    
+  const completeTask = id => {
+    const updatedArrayTasks = arrayTasks.map(task => {
+      if (task.id === id) {
+        task.completed = !task.completed;
+      }
+      return task;
+    });
+    setArrayTasks(updatedArrayTasks);
+    alert("Task completed successfully!");
   };
 
-  const removeTask = (id) => {
-    
+  const removeTask = id => {
+    const updatedArrayTasks = arrayTasks.filter(task => task.id !== id);
+    setArrayTasks(updatedArrayTasks);
+    alert("Task removed successfully!")
   };
 
   return (
-    <>
+    //No se crea un div como tal, sino un fragmento de React con <></>
+    <> 
       <Form onSubmit={addTask} />
       <div className='task-list-container'>
         {
