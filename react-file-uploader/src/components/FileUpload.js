2cb19573aa1f7eb8911b899984c016fded74eb75
MissiegoBeats <missiego.andres@gmail.com>
2023-06-28 12:20:12 +0200

    Not finished, changing to another project

--- react-file-uploader/src/components/FileUpload.js
+++ react-file-uploader/src/components/FileUpload.js
@@ -19,9 +19,9 @@ function FileUpload() {
       method: 'POST',
       body: formData
     })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+      .then((response) => response.json())
+      .then((result) => console.log('Success:', result))
+      .catch(error => console.error('Error: ', error));
   }
 
   return (
