0a7505f7d75a11fde3d3c08e72ab960ce61a650a
MissiegoBeats <missiego.andres@gmail.com>
2023-06-29 10:36:10 +0200

    developing 3d web (from tutorial)

--- 3D-project/src/App.jsx
+++ 3D-project/src/App.jsx
@@ -1,7 +1,34 @@
+import { BrowserRouter } from 'react-router-dom'
+import About from './components/About'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Feedbacks from './components/Feedbacks'
+import Hero from './components/Hero'
+import NavBar from './components/NavBar'
+import StarsCanvas from './components/StarsCanvas'
+import Tech from './components/Tech'
+import Works from './components/Works'
+
 const App = () => {
   return (
     <div>
-      3D Project
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <NavBar />
+            <Hero />
+          </div>
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
