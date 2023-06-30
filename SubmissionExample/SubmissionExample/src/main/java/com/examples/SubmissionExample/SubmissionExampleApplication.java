28d41caeafc08aa5439f76c04f9ecf028707835f
MissiegoBeats <missiego.andres@gmail.com>
2023-06-30 12:52:33 +0200

    added spring x react project

---
+++ SubmissionExample/SubmissionExample/src/main/java/com/examples/SubmissionExample/SubmissionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.examples.SubmissionExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionExampleApplication.class, args);
+	}
+
+}
