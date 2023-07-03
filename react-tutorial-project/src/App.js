b66dadd9d22e4c0eaee12a9c71de1ef96e8a9e66
MissiegoBeats <missiego.andres@gmail.com>
2023-07-03 12:48:04 +0200

    Some changes

--- react-tutorial-project/src/App.js
+++ react-tutorial-project/src/App.js
@@ -1,10 +1,13 @@
 import './styles/App.css';
 import { useRef } from 'react';
+import image from './images/img.png';
+import image2 from './images/img2.png';
 
 function App() {
 
   const change = 23.16;
   const numberHolder = useRef('number-holder');
+  var changed = false;
 
   function increment(e) {
     e.target.innerHTML = parseInt(e.target.innerHTML) + 1;
@@ -21,13 +24,36 @@ function App() {
     numberHolder.current.style.backgroundColor = "#fff";
   }
 
+  function changeImg(j) {
+    if (changed) {
+      j.target.src = image;
+      changed = false;
+    } else {
+      j.target.src = image2;
+      changed = true;
+    }
+  }
+
+  function readChange(k) {
+    numberHolder.current.innerHTML = k.target.value;
+    if(k.target.value === "") {
+      numberHolder.current.innerHTML = 1;
+    }
+  }
+
   return (
     <div className="App">
       <div ref={numberHolder} className="number-holder" onClick={increment}>
         1
       </div>
-      <button onClick={convert}>Convert</button>
-      <button onClick={reset}>Reset</button>
+      <div>
+        <button onClick={convert}>Convert</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <img src={image} height={200} width={200} onClick={changeImg} />
+      </div>
+      <input type="text" placeholder="Enter your name" onChange={readChange}/>
     </div>
   );
 }
