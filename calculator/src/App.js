a9b6e9456d64a4e9f6fc9c08b23081522e71bcae
MissiegoBeats <missiego.andres@gmail.com>
2023-07-04 10:39:00 +0200

    Developing

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,66 +1,10 @@
 import './App.css';
-import Button from './components/Button.js';
-import Screen from './components/Screen.js';
-import ClearButton from './components/ClearButton';
-import { useState } from 'react';
-import { evaluate } from 'mathjs';
+import Calculator from './Calculator';
 
 function App() {
-
-  const [input, setInput] = useState('');
-
-  const addToInput = val => {
-    setInput(input + val);
-  };
-
-  const clearInput = () => {
-    setInput('');
-  };
-
-  const processOperation = () => {
-    if (input) {
-      setInput(evaluate(input));
-    } else {
-      alert("You must introduce some mathematical expression");
-    }
-  };
-
   return (
     <div className="App">
-      <div className='calculator-container'>      
-        <div className='row'>
-          <Screen
-            input = { input }
-          />
-        </div>
-        <div className='row'>
-          <Button handleClick={addToInput}>1</Button>
-          <Button handleClick={addToInput}>2</Button>
-          <Button handleClick={addToInput}>3</Button>
-          <Button handleClick={addToInput}>+</Button>
-        </div>
-        <div className='row'>
-          <Button handleClick={addToInput}>4</Button>
-          <Button handleClick={addToInput}>5</Button>
-          <Button handleClick={addToInput}>6</Button>
-          <Button handleClick={addToInput}>-</Button>
-        </div>
-        <div className='row'>
-          <Button handleClick={addToInput}>7</Button>
-          <Button handleClick={addToInput}>8</Button>
-          <Button handleClick={addToInput}>9</Button>
-          <Button handleClick={addToInput}>*</Button>
-        </div>
-        <div className='row'>
-          <Button handleClick={addToInput}>.</Button>
-          <Button handleClick={addToInput}>0</Button>
-          <Button handleClick={processOperation}>=</Button>
-          <Button handleClick={addToInput}>/</Button>
-        </div>
-        <div className='row'>
-          <ClearButton handleClick={clearInput}>Clear</ClearButton>
-        </div>
-      </div>
+      <Calculator />
     </div>
   );
 }
--- my-web/src/App.js
+++ my-web/src/App.js
@@ -1,10 +1,15 @@
 import './styles/App.css';
 import Banner from './components/Banner.js';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <div className="calculator">
+        <Calculator />
+      </div>
+      <div className="calculator-info"><p>This calculator was made with React!!</p></div>
     </div>
   );
 }
