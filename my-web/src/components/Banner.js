484d0d60d44de95c4701ddaca177e1b949bd4043
MissiegoBeats <missiego.andres@gmail.com>
2023-07-04 12:38:48 +0200

    Added Contact section

--- my-web/src/components/Banner.js
+++ my-web/src/components/Banner.js
@@ -1,11 +1,14 @@
 import React from "react";
 import '../styles/Banner.css';
+import { GrContact } from 'react-icons/gr';
+import { useNavigate } from "react-router-dom";
 
 function Banner() {
   return (
-    <div className="banner">
+    <div className="banner" >
       <h1>My Web</h1>
       <p>My React App</p>
+      <div className='contact'><a className='contact-button' href="#contact-div">Contact &nbsp;| &nbsp;<GrContact /></a></div>
     </div>
   );
 }
