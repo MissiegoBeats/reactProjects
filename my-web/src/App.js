6d5d5e7d0e4e5cf6206054a5e7cf61d41143a24b
MissiegoBeats <missiego.andres@gmail.com>
2023-07-04 08:56:27 +0200

    Making a project by my own

---
+++ my-web/src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import Banner from './components/Banner.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
