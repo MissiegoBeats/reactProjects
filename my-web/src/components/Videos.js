5c453f14e661e6e46072eb1f2e162825f6f57475
MissiegoBeats <missiego.andres@gmail.com>
2023-07-04 14:04:26 +0200

    added videos section

--- my-web/src/components/Videos.js
+++ my-web/src/components/Videos.js
@@ -4,27 +4,33 @@ import '../styles/Videos.css';
 function Videos() {
   return (
     <div className="videos">
-      <iframe
-        src="https://www.youtube.com/embed/UsR08cY8k0A"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen>
-      </iframe>
-      <iframe
-        src="https://www.youtube.com/embed/lsYiPIGwehc"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen>
-      </iframe>
-      <iframe
-        src="https://www.youtube.com/embed/ga3wfFtloH0"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen>
-      </iframe>
+      <div class="iframe-container">
+        <iframe
+          src="https://www.youtube.com/embed/UsR08cY8k0A"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen>
+        </iframe>
+      </div>
+      <div class="iframe-container">
+        <iframe
+          src="https://www.youtube.com/embed/lsYiPIGwehc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen>
+        </iframe>
+      </div>
+      <div class="iframe-container">
+        <iframe
+          src="https://www.youtube.com/embed/ga3wfFtloH0"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen>
+        </iframe>
+      </div>
     </div>
   );
 }
