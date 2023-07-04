ba652116420c0988d73d1a71b9f5f146d4b441e3
MissiegoBeats <missiego.andres@gmail.com>
2023-07-04 13:47:31 +0200

    Edited scrollbar and added embeded videos

--- my-web/src/App.js
+++ my-web/src/App.js
@@ -2,6 +2,7 @@ import './styles/App.css';
 import Banner from './components/Banner.js';
 import Calculator from './components/Calculator';
 import ContactInfo from './components/ContactInfo';
+import Videos from './components/Videos';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Calculator />
       </div>
       <div className="calculator-info"><p>This calculator was made with React!!</p></div>
+      <Videos />
       <div className="contact-info" id="contact-div">
         <ContactInfo />
       </div>
