e188e3eaec66e7438e7b74db9a3383d09deed81b
MissiegoBeats <missiego.andres@gmail.com>
2023-07-05 14:37:07 +0200

    Added description

--- artist-portfolio/src/components/About.js
+++ artist-portfolio/src/components/About.js
@@ -7,9 +7,13 @@ import '../styles/About.css'; // Import the custom CSS file
 
 function About() {
   return (
-    <div className="bg-dark text-white" style={{ marginTop: '60px', marginBottom: '20px', userSelect:"none", height:"550px"}} id="about">
+    <div className="bg-dark text-white" style={{ marginTop: '60px', marginBottom: '20px', userSelect:"none", height:"650px"}} id="about">
       <div className="text-center mb-4 mt-2" style={{borderBottom:"2px solid white", display:"flex", flexWrap:"wrap", width:"100%", justifyContent:"center", alignItems:"center"}}>
         <h1>About</h1>
+      </div>
+      <div className="text-center text-justify" style={{ marginTop: "20px", alignItems:"center"}}>
+        <p>20yo Spanish producer/singer trying to make his way into the music industry.His main goal is to help people get out of <br /> bad times with my music. A very polyvalent artist that makes pop-rock/pop-punk, ballads, emo rap, <br />hip hop, melodic rap, hard hitting rap... 
+          You can listen to this versatility and passion for music in his discography</p>
       </div>
       <div className="d-flex justify-content-center">
         <div className="card m-2 bg-dark text-white hover-animation" style={{ width: '18rem' }}>
