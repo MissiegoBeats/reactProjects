e8429f56868101e06df3962cbd1b1a240260d0ba
MissiegoBeats <missiego.andres@gmail.com>
2023-07-05 12:55:06 +0200

    Added new project

---
+++ artist-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ artist-portfolio/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+}
+
+export default Contact;
