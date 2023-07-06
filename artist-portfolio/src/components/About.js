7f6696ca5900c95e7316240672020dba03d9d7a6
MissiegoBeats <missiego.andres@gmail.com>
2023-07-06 12:21:11 +0200

    Developing

--- artist-portfolio/src/components/About.js
+++ artist-portfolio/src/components/About.js
@@ -5,12 +5,13 @@ import image2 from '../images/image2.png';
 import image3 from '../images/image3.png';
 import '../styles/About.css'; // Import the custom CSS file
 import Card from './Card.js';
+import PhotoSlider from './PhotoSlider';
 
 function About() {
   return (
-    <div className="bg-dark text-white" style={{ marginTop: '60px', marginBottom: '20px', userSelect:"none", height:"650px"}} id="about">
+    <div className="bg-dark text-white" style={{ marginTop: '-10px', marginBottom: '0px', userSelect:"none", height:"auto"}}>
       <div className="text-center mb-4 mt-2" style={{borderBottom:"2px solid white", display:"flex", flexWrap:"wrap", width:"100%", justifyContent:"center", alignItems:"center"}}>
-        <h1>About</h1>
+        <h1>About me</h1>
       </div>
       <div className="text-center text-justify" style={{ marginTop: "20px", alignItems:"center"}}>
         <p>20yo Spanish producer/singer trying to make his way into the music industry.His main goal is to help people get out of <br /> bad times with my music. A very polyvalent artist that makes pop-rock/pop-punk, ballads, emo rap, <br />hip hop, melodic rap, hard hitting rap... 
@@ -32,6 +33,9 @@ function About() {
           name="Thoughts"
           text="Some quick example text to build on the card title and make up the bulk of the card's content."
           url="https://youtu.be/1zvGHLummI4" />
+      </div>
+      <div className="photo-slider bg-dark text-white d-flex justify-content-center align-items-center" style={{ height: 'auto', width: '100%', marginTop:"20px"}}>
+        <PhotoSlider />
       </div>
     </div>
   );
--- artist-portfolio/src/components/Menu.js
+++ artist-portfolio/src/components/Menu.js
@@ -5,14 +5,13 @@ import logoMenu from '../images/logo-menu.png';
 
 function Menu() {
   return (
-    <Navbar className="navbar fixed-top" bg="dark" expand="lg" data-bs-theme="dark">
+    <Navbar className="navbar fixed-top border-bottom border-light" bg="dark" expand="lg" data-bs-theme="dark">
       <Navbar.Brand href="#home" style={{ marginLeft: "10px" }}><img src={logoMenu} height={40} width={40}></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto"> {/* me-auto = margin-left: auto */}
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
             <NavDropdown title="More" id="basic-nav-dropdown"> {/* id is required */}
               <NavDropdown.Item href="#albums">Albums</NavDropdown.Item> {/* href is required */}
               <NavDropdown.Item href="#single">Latest Single</NavDropdown.Item> {/* href is required */}
@@ -20,6 +19,7 @@ function Menu() {
               <NavDropdown.Divider /> {/* Divider is optional */}
               <NavDropdown.Item href="#social-media">Social Media</NavDropdown.Item> {/* href is required */}
             </NavDropdown>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
